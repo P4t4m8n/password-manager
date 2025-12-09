17b18b5290529304085fea896e02447144b86b5c
Eran <michaelieran@gmail.com>
2025-12-09 13:22:40 +0200

    Add after updfate component

---
+++ frontend/app/src/app/features/Auth/pages/after-signup-index/after-signup-index.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-after-signup-index',
+  imports: [],
+  templateUrl: './after-signup-index.html',
+  styleUrl: './after-signup-index.css',
+})
+export class AfterSignupIndex {
+
+}
