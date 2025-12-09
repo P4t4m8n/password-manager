2fcd0ae251b6c7a1ef98e5971180d4711938971d
Eran <michaelieran@gmail.com>
2025-12-09 13:22:24 +0200

    Move auth route into file update file names

--- frontend/app/src/app/app.routes.ts
+++ frontend/app/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './core/layout/main/main-root/main-layout';
-import { PASSWORD_ENTRIES_ROUTES } from './features/password-entry/consts/routes.const';
+import { PASSWORD_ENTRIES_ROUTES } from './features/password-entry/consts/password-entry-routes.const';
 import { PASSWORD_GENERATOR_ROUTES } from './features/password-generator/consts/password-generator-routes.const';
 import { SETTINGS_ROUTES } from './features/settings/const/settings-routes.const';
 import { authGuard } from './features/auth/guards/auth-guard';
 import { MASTER_PASSWORD_ROUTES } from './features/master-password/const/master-password-routes.const';
 import { AuthIndex } from './features/auth/pages/auth-index/auth-index';
+import { AUTH_ROUTES } from './features/auth/consts/auth-routes.const';
 
 export const routes: Routes = [
   {
@@ -20,6 +21,6 @@ export const routes: Routes = [
       ...MASTER_PASSWORD_ROUTES,
     ],
   },
-  { path: 'auth', component: AuthIndex },
+  ...AUTH_ROUTES,
   { path: '**', redirectTo: '' },
 ];
