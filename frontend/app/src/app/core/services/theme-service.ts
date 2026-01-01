13fd4123ec2971f84b631a02fb696744c1ced587
Eran <michaelieran@gmail.com>
2026-01-01 17:05:24 +0200

    password entries style finished

--- frontend/app/src/app/core/services/theme-service.ts
+++ frontend/app/src/app/core/services/theme-service.ts
@@ -68,7 +68,7 @@ export class ThemeService extends AbstractGlobalStateService<TTheme | null> {
   private applyTheme(theme: Omit<TTheme, 'system'>): void {
     const root = document.documentElement;
     // if (theme === 'dark') {
-      root.classList.add('dark-theme');
+      // root.classList.add('dark-theme');
     // } else {
     //   root.classList.remove('dark-theme');
     // }
