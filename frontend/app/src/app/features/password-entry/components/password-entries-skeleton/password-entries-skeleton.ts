3801c931afdb6524cdb2f1ffdc7de972d6cafa41
Eran <michaelieran@gmail.com>
2026-01-09 15:04:15 +0200

    Add loading skeleton to password entries

---
+++ frontend/app/src/app/features/password-entry/components/password-entries-skeleton/password-entries-skeleton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-entries-skeleton',
+  imports: [],
+  templateUrl: './password-entries-skeleton.html',
+  styleUrl: './password-entries-skeleton.css',
+})
+export class PasswordEntriesSkeleton {
+
+}
--- frontend/app/src/app/features/password-entry/pages/password-entries/password-entries.ts
+++ frontend/app/src/app/features/password-entry/pages/password-entries/password-entries.ts
@@ -3,20 +3,30 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
-import { debounceTime, distinctUntilChanged, Observable, Subscription, switchMap, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  finalize,
+  Observable,
+  Subscription,
+  switchMap,
+  tap,
+} from 'rxjs';
 
 import { PASSWORD_ENTRIES_PATHS } from '../../consts/password-entry-routes.const';
 
 import { PasswordEntryHttpService } from '../../services/password-entry-http-service';
 import { ErrorService } from '../../../../core/services/error-service';
 
-import { IconSearch } from '../../../../core/icons/icon-search/icon-search';
-import { IconPlus } from '../../../../core/icons/icon-plus/icon-plus';
-import { PasswordEntryTable } from '../../components/password-entry-table/password-entry-table';
 import { Header } from '../../../../core/layout/header/header';
+import { PasswordEntriesSafety } from '../../components/password-entries-safety/password-entries-safety';
+import { PasswordEntryTable } from '../../components/password-entry-table/password-entry-table';
+import { PasswordEntriesSkeleton } from '../../components/password-entries-skeleton/password-entries-skeleton';
+import { IconPlus } from '../../../../core/icons/icon-plus/icon-plus';
+import { IconSearch } from '../../../../core/icons/icon-search/icon-search';
 
 import type { IPasswordEntryDto } from '../../interfaces/passwordEntry';
-import { PasswordEntriesSafety } from "../../components/password-entries-safety/password-entries-safety";
 @Component({
   selector: 'app-password-entities',
   imports: [
@@ -27,8 +37,9 @@ import { PasswordEntriesSafety } from "../../components/password-entries-safety/
     PasswordEntryTable,
     ReactiveFormsModule,
     Header,
-    PasswordEntriesSafety
-],
+    PasswordEntriesSafety,
+    PasswordEntriesSkeleton,
+  ],
   templateUrl: './password-entries.html',
   styleUrl: './password-entries.css',
 })
@@ -45,6 +56,9 @@ export class PasswordEntries implements OnInit, OnDestroy {
   public passwordEntries$: Observable<IPasswordEntryDto[] | null> =
     this.#passwordEntryHttpService.data$;
 
+  #isLoading = new BehaviorSubject<boolean>(false);
+  public isLoading$ = this.#isLoading.asObservable();
+
   public passwordEntriesPaths = PASSWORD_ENTRIES_PATHS;
 
   public searchControl = new FormControl('');
@@ -59,13 +73,19 @@ export class PasswordEntries implements OnInit, OnDestroy {
             if (this.searchControl.value !== entryName) {
               this.searchControl.setValue(entryName, { emitEvent: false });
             }
+            this.#isLoading.next(true);
 
-            return this.#passwordEntryHttpService.get({ entryName, isLiked });
+            return this.#passwordEntryHttpService.get({ entryName, isLiked }).pipe(
+              finalize(() => this.#isLoading.next(false))
+            );
           })
         )
         .subscribe({
           error: (err) => {
             this.#errorService.handleError(err, { showToast: true });
+          },
+          complete: () => {
+            // this.#isLoading.next(false);
           },
         })
     );
