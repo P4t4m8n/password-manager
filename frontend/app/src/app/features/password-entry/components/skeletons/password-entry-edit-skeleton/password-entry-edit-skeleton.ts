0a1b58055b92bf0d029228d05afd39f18df27c89
Eran <michaelieran@gmail.com>
2026-01-10 15:28:22 +0200

    Add password entry edit skeleton for mobile and large screens

---
+++ frontend/app/src/app/features/password-entry/components/skeletons/password-entry-edit-skeleton/password-entry-edit-skeleton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-password-entry-edit-skeleton',
+  imports: [],
+  templateUrl: './password-entry-edit-skeleton.html',
+  styleUrl: './password-entry-edit-skeleton.css',
+})
+export class PasswordEntryEditSkeleton {
+
+}
