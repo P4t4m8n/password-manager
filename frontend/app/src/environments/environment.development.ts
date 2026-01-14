5b3b41f62a1569e9336f63a66fb88cefa1b9b7f5
Eran <michaelieran@gmail.com>
2026-01-14 19:25:30 +0200

    a

---
+++ frontend/app/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5222/api',
+  pbkdf2Iterations: 100000,
+  enableErrorLogging: true,
+};
---
+++ frontend/app/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://secure-api-bbhafbe0a2fsexe4.israelcentral-01.azurewebsites.net/api',
+  pbkdf2Iterations: 100000,
+  enableErrorLogging: false,
+};
